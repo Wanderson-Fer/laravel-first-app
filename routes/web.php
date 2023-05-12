997c10d7358e46cb8fa50a28dcb9331fcd64e5b6
Wanderson <wanderson.wf@pg.com>
2023-05-12 10:11:27 -0400

    reorganizing... focused on jogos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\homeController;
 use App\Http\Controllers\jogosController;
 
 use Illuminate\Support\Facades\Route;
@@ -16,17 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
-
-// Route::get('/home/{name?}', function ($name=null) {
-//     return view('home', ['nome'=>$name]);
-// })->where('name', '[A-Za-z]+');
-
-Route::get('/home', [homeController::class, 'index'])->name('home.index');
-
-Route::get('/jogos', [jogosController::class, 'index'])->name('jogos.index');
+// Route::get('/jogos', [jogosController::class, 'index'])->name('jogos.index');
 
 Route::fallback(function () {
     return view('fail');
